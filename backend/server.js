07be44fdf55a8a9b1baa598311e9889b7d1c9479
Jai Saini <jaisaini6167@gmail.com>
2026-01-25 23:31:49 +0530

    Cors Updated

--- backend/server.js
+++ backend/server.js
@@ -16,11 +16,11 @@ dotenv.config()
 const app = express()
 app.use(cookieParser());
 app.use(express.json())
-// app.use(cors({
-//   origin: process.env.CLIENT_URL,
-//   credentials: true 
-// }))
-app.use(cors())
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true 
+}))
+// app.use(cors())
 
 
 app.use('/api/auth', authRoutes);
