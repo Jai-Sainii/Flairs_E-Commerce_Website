10e0971db73c673cf535d7ee3fac0a86003f1200
Jai-Sainii <jaisaini6167@gmail.com>
2026-01-29 13:26:28 +0530

    Auth Cookies Updated

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -33,7 +33,7 @@ export const signup = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", 
-      sameSite: "Strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, 
     });
 
@@ -76,7 +76,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", 
-      sameSite: "Strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, 
     });
 
